80de7c4c4db2d8c5db7e2d0a671e462c512c59fc
Glitch (probotfake) <none>
2020-03-25 15:04:48 +0000

    🎙🔑 Checkpoint ./server.js:6566634/1477 ./config.json:6566634/401

--- server.js
+++ server.js
@@ -404,7 +404,7 @@ Join the support server at discord.gg/ProBot.
 
 client.on("message", message => {
   if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("Pong...").then(msg => {
+    message.channel.send("Pong").then(msg => {
       msg.edit(
           `\`\`\`javascript\nTime taken: ${msg.createdTimestamp -
             message.createdTimestamp} ms.\nDiscord API: ${Math.round(
@@ -414,3 +414,51 @@ client.on("message", message => {
     });
   }
 });
+
+const config = require('./config.json');
+
+const size    = config.colors;
+const rainbow = new Array(size);
+
+for (var i=0; i<size; i++) {
+  var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
+  var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
+  var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
+
+  rainbow[i] = '#'+ red + green + blue;
+}
+
+function sin_to_hex(i, phase) {
+  var sin = Math.sin(Math.PI / size * 2 * i + phase);
+  var int = Math.floor(sin * 127) + 128;
+  var hex = int.toString(16);
+
+  return hex.length === 1 ? '0'+hex : hex;
+}
+
+let place = 0;
+const servers = config.servers;
+
+function changeColor() {
+  for (let index = 0; index < servers.length; ++index) {		
+    client.guilds.get(servers[index]).roles.find('name', config.roleName).setColor(rainbow[place])
+		.catch(console.error);
+		
+    if(config.logging){
+      console.log(`[ColorChanger] Changed color to ${rainbow[place]} in server: ${servers[index]}`);
+    }
+    if(place == (size - 1)){
+      place = 0;
+    }else{
+      place++;
+    }
+  }
+}
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+  setInterval(changeColor, config.speed);
+});
+
+
+client.login(config.token);

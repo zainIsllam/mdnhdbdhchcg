0937052f9dba0fe7f55ceb136d9b604b01e89a12
Glitch (probotfake) <none>
2020-04-15 20:08:24 +0000

    🛤💑 Checkpoint ./config.js:6566634/118

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    TOKEN: 'NjkwOTU3MTgxNTA3NjAwMzk1.XoI6oQ.m-pwejsDqn8b71L6pwVINAYnL94', 
+    TOKEN: 'NjkwOTU3MTgxNTA3NjAwMzk1.XpdnzA.xcXWhLbpgEhzrZomt598a-AbzP0', 
     prefix: '#',
 }
